bda5c337e231dc07f8ffd96edd87295412c1c0a9
Kawish Qayyum <kawishqayyum@gmail.com>
2024-07-29 16:46:33 +0500

    Refactor: Add newlines to improve readability

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -10,17 +10,24 @@ class QuotesSpider (scrapy.Spider):
 
     def parse(self, response):
         self.logger.info('This is my first Spider.')
+
         # quotes = response.css('div.quote')
         quotes = response.xpath("//div[@class='quote']")
+
         for quote in quotes:
             loader = ItemLoader(item=QuoteItem(), selector=quote)
+
             # loader.add_css('quote_content', '.text::text')
             loader.add_xpath('quote_content', '//*[@class="text"]/text()')
+
             # loader.add_css('tags', '.tags > a::text')
             loader.add_xpath('tags', './/*[@class="tags"]//a/text()')
+
             quote_item = loader.load_item()
+
             # author_url = quote.css('.author + a::attr(href)').get()
             author_url = quote.xpath("//*[@class='quote']//a/@href").get()
+
             yield response.follow(
                 author_url, self.parse_author, meta={'quote_item': quote_item}
             )
@@ -34,18 +41,28 @@ class QuotesSpider (scrapy.Spider):
     def parse_author(self, response):
         quote_item = response.meta['quote_item']
         loader = ItemLoader(item=quote_item, response=response)
+
         # loader.add_css('author_name', '.author-title::text')
-        loader.add_xpath('author_name', '//*[@class="author-title"]/text()')
+        loader.add_xpath(
+            'author_name', '//*[@class="author-title"]/text()'
+        )
+
         # loader.add_css('author_birthday', '.author-born-date::text')
-        loader.add_xpath('author_birthday',
-                         '//*[@class="author-born-date"]/text()'
-                         )
+        loader.add_xpath(
+            'author_birthday',
+            '//*[@class="author-born-date"]/text()'
+        )
+
         # loader.add_css('author_bornlocation', '.author-born-location::text')
-        loader.add_xpath('author_bornlocation',
-                         '//*[@class="author-born-location"]/text()'
-                         )
+        loader.add_xpath(
+            'author_bornlocation',
+            '//*[@class="author-born-location"]/text()'
+        )
+
         # loader.add_css('author_bio', '.author-description::text')
-        loader.add_xpath('author_bio',
-                         '//*[@class="author-description"]/text()'
-                         )
+        loader.add_xpath(
+            'author_bio',
+            '//*[@class="author-description"]/text()'
+        )
+
         yield loader.load_item()
